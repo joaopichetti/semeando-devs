d233fd59ae7a72c9db14f9f2b8df92a6359deb26
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2023-09-05 20:59:20 -0300

    Primeiro Widget

---
+++ flutter_hello_world/lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MeuApp());
+}
+
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text(
+        'Minha Aplicação',
+        textDirection: TextDirection.ltr,
+      ),
+    );
+  }
+}
