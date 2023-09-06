4161239c47fb5b7dbd768e2ed00aa7e9ac13f962
João Guilherme Brasil Pichetti <joaogbpichetti@gmail.com>
2023-09-05 21:37:59 -0300

    Adicionando Column no HomePage

--- flutter_hello_world/lib/home_page.dart
+++ flutter_hello_world/lib/home_page.dart
@@ -10,34 +10,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   var count = 0;
 
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Text(
-          'Você clicou $count vezes no Floating Action Button',
-          style: TextStyle(
-            fontSize: 30,
-            fontStyle: FontStyle.italic,
-            fontWeight: FontWeight.bold,
+      body: Column(
+        children: [
+          Container(
+            height: 80,
+            width: 80,
+            color: Colors.red,
+          ),
+          Container(
+            height: 80,
+            width: 80,
             color: Colors.blue,
           ),
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(
-          Icons.add,
-        ),
-        onPressed: () {
-          setState(() {
-            count++;
-          });
-          print('count: $count');
-        },
+          Container(
+            height: 80,
+            width: 80,
+            color: Colors.green,
+          ),
+        ],
       ),
     );
   }
